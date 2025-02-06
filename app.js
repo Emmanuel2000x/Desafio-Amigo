aac0f7369595cc32a9aff019e567fb7b0695df9e
Emmanuel <s.emmanuel2000@gmail.com>
2025-02-05 21:54:40 -0600

    Se agrega el array de amigos y verificacion de input

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let friends = []
+
+function agregarAmigo(){
+    let newFriend = document.getElementById('friend').value;
+    if (newFriend == "") {
+        alert("Porfavor agrega un nombre");
+        return false;
+    } else{
+        friends.push(newFriend);
+        console.log(friends),
+        clearBox();
+    }
+      
+}
+
+
+
+function clearBox(){
+    document.querySelector('#friend').value = '';
+}
